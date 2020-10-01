ea3cb677f7092262463134443bd536b5201891ff
Lamington1010 <57505019+Lamington1010@users.noreply.github.com>
2020-09-30 21:15:57 -0400

    Updated Script
    Completed Python Script for importing 'm' number of images (n, n), and saving as array of shape (m, n^2)

--- importimagespython.py
+++ importimagespython.py
@@ -6,23 +6,29 @@ Created on Sun Sep 20 18:23:01 2020
 """
 import cv2
 import numpy as np
-import os
 
 def imgimport():
     s1 = input("image filename without numbers, e.g. cats")
     s2 = input("file type, e.g. .jpg, .tif, etc ")
     s3 = int(input("what is the range of images?")) 
+    s3a = 'file directory'
     
     x = 1; 
-    Arr = 0;
+    Matrix = [];
   
     while x < s3:
         s4 = str(x);
-        s5 = s1+s4+s2
+        s5 = s3a+s1+s4+s2
         img = cv2.imread(s5,cv2.IMREAD_GRAYSCALE);
         img = np.array(img)
         imgprime = np.transpose(img)
         imgprimecol = imgprime.flatten()
-        imgprimenowcol = imgprimecol.reshape(len(img),1)
-        Arr = Arr.append(imgprimenowcol);
+       
+        Arr = np.reshape(imgprimecol,(len(imgprimecol),1))
+        Matrix.append(Arr);
         x = x + 1;
+        print(np.size(Matrix))
+    
+    A = np.shape(Matrix)
+        
+    return np.reshape(Matrix,(A[0],A[1]))
